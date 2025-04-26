6c0829b63d40661c225881c985ff77311714825b
rizqiau <mochammadrizqiaullia@gmail.com>
2025-04-26 22:37:37 +0700

    resolve error

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -18,9 +18,11 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
