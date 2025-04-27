7dcfaa207ab6ea1194ab2da5c2f147e8cf3796d3
rizqiau <mochammadrizqiaullia@gmail.com>
2025-04-27 20:41:51 +0700

    update obfuscation gradle app & core

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -19,17 +19,9 @@ android {
     buildTypes {
         debug {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
